88d1e2a3bbd68d61f0c472547bff75f198473132
NicoMarshall <nico.marshall@btinternet.com>
2022-08-18 14:20:49 +0100

    methods added

--- waterstones_scraping.py
+++ waterstones_scraping.py
@@ -7,5 +7,20 @@ class Scraper:
     def __init__(self, driver, url) :
         self.driver = driver
         self.url = url
+        url_list = []
+    
+    def go_to(url)  :
+        driver.get(url)
+        time.sleep(2)
+        
+    def next_page() :
+       time.sleep(2)
+       next_button = driver.find_element(By.CLASS_NAME,"next")
+       next_button.click()    
+    
+    def accept_cookies() :
+        accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
+        accept_cookies_button.click()
+  
         
         
