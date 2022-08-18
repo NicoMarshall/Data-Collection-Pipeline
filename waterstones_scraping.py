8fb1035221e45718428aae4d06e1f9e9c4a52107
NicoMarshall <nico.marshall@btinternet.com>
2022-08-18 15:56:37 +0100

    gather_links method added

--- waterstones_scraping.py
+++ waterstones_scraping.py
@@ -10,17 +10,26 @@ class Scraper:
         url_list = []
     
     def go_to(url)  :
-        driver.get(url)
-        time.sleep(2)
+        time.sleep(4)
+        self.driver.get(url)
+        
         
     def next_page() :
-       time.sleep(2)
-       next_button = driver.find_element(By.CLASS_NAME,"next")
+       time.sleep(4)
+       next_button = self.driver.find_element(By.CLASS_NAME,"next")
        next_button.click()    
     
     def accept_cookies() :
         accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
         accept_cookies_button.click()
   
-        
+    def gather_links() :
+        global url_list
+        page_results = self.driver.find_element(by=By.XPATH, value='//div[@class="search-results-list"]')
+        book_list = page_results.find_elements(by=By.XPATH, value='./div')
+        for book in book_list:
+            book_image = book.find_element(By.CLASS_NAME,"image-wrap")
+            a_tag = book_image.find_element(By.TAG_NAME,"a")
+            link = a_tag.get_attribute("href")
+            url_list.append(link)
         
