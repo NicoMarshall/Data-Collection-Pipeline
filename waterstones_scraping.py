48f6e7953759294fb43debb67c105b23e73b4896
NicoMarshall <nico.marshall@btinternet.com>
2022-08-25 14:55:27 +0100

    data stored locally

--- waterstones_scraping.py
+++ waterstones_scraping.py
@@ -3,6 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import uuid
+import json
+import os
+try:
+    os.mkdir("raw_data")
+    print("Directory raw_data created ") 
+except FileExistsError:
+    print("Directory raw_data already exists")
 
 class Scraper:
     def __init__(self, driver, url) :
@@ -42,9 +49,20 @@ class Scraper:
         book_description_2 = self.driver.find_element(by=By.XPATH,value ='//div[@itemprop="description"]/p[2]').get_attribute("textContent")
         book_description = book_description_1 +  book_description_2
         isbn = self.driver.find_element(by=By.XPATH,value ='//span[@itemprop="isbn"]').get_attribute("textContent")
-        book_dict = {"book title" : book_title, "book_author" : book_author, "book_price" : book_price, "book_description" : book_description, "isbn" : isbn, "uuid" : uuid.uuid4()}
+        book_dict = {"book title" : book_title, "book_author" : book_author, "book_price" : book_price, "book_description" : book_description, "isbn" : isbn, "uuid" : str(uuid.uuid4())}
         self.book_data[f"{isbn}"] = book_dict
-            
+        os.chdir("C:/Users/Home\Data Collection Pipeline/Data-Collection-Pipeline/raw_data")
+        try:
+            os.mkdir(f"{isbn}")
+        except FileExistsError:
+            pass
+        os.chdir(f"C:/Users/Home/Data Collection Pipeline/Data-Collection-Pipeline/raw_data/{isbn}")
+        with open('data.json', 'w') as convert_file:
+            convert_file.write(json.dumps(book_dict))
+        os.chdir("C:/Users/Home/Data Collection Pipeline/Data-Collection-Pipeline")   
+        
+        
+        
 if __name__ == '__main__':
     url = "https://www.waterstones.com/category/science-technology-medicine/page/1"
     options = webdriver.ChromeOptions()
@@ -60,7 +78,7 @@ if __name__ == '__main__':
     for book_url in waterstones_scraper.url_list :
         waterstones_scraper.go_to(book_url)    
         waterstones_scraper.scrape_text()
-        print(waterstones_scraper.book_data)
+        
  
     driver.quit()    
     
