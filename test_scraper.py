2a6ba75e6ff00fca5555f4a2e4b65496f14aaa45
NicoMarshall <nico.marshall@btinternet.com>
2022-09-12 16:16:57 +0100

    added test_scrape_text

--- test_scraper.py
+++ test_scraper.py
@@ -1,6 +1,8 @@
 from nturl2path import url2pathname
+from secrets import choice
 import unittest
 import os.path
+import random
 from selenium import webdriver
 from waterstones_scraping import Scraper
 
@@ -20,11 +22,24 @@ class ScraperTestCase(unittest.TestCase):
         self.scraper.create_data_directory()
         actual_value = os.path.exists("raw_data")
         self.assertEqual(expected_value, actual_value)
+       
         
-    def test_gather_links(self):
-        self.scraper.go_to(url)
+    #def test_gather_links(self):
+        #self.scraper.go_to(url)
+        #self.scraper.accept_cookies()
+        #self.scraper.gather_links()
+        #self.assertGreaterEqual(len(self.scraper.url_list), 5)
+        
+    def test_scrape_text(self):
+        self.scraper.go_to("https://www.waterstones.com/book/a-brief-history-of-time/stephen-hawking/9780857501004")
         self.scraper.accept_cookies()
-        self.scraper.gather_links()
-        self.assertGreaterEqual(len(self.scraper.url_list), 5)
+        self.scraper.scrape_text()    
+        book_dictionary = self.scraper.book_data[f"{9780857501004}"]
+        expected_title = "A Brief History Of Time: From Big Bang To Black Holes (Paperback)"
+        actual_title = book_dictionary["book_title"]
+        self.assertEqual(expected_title, actual_title)
+        expected_author = "Stephen Hawking"
+        actual_author = book_dictionary["book_author"]
+        self.assertEqual(expected_author, actual_author)
         
         
